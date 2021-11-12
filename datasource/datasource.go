37f737f292f2bc0a16f657539802777366aa90e7
feitian124 <feitian124@gmail.com>
2021-11-12 21:32:50 +0800

    rm useless code

--- datasource/datasource.go
+++ datasource/datasource.go
@@ -1,14 +1,7 @@
 package datasource
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/feitian124/goapi/config"
 	"github.com/feitian124/goapi/database/drivers"
@@ -21,7 +14,6 @@ import (
 	"github.com/xo/dburl"
 )
 
-// Analyze database
 func Analyze(dsn config.DSN) (*schema.Schema, error) {
 	urlstr := dsn.URL
 	s := &schema.Schema{}
@@ -85,77 +77,6 @@ func Analyze(dsn config.DSN) (*schema.Schema, error) {
 	err = driver.Analyze(s)
 	if err != nil {
 		return s, err
-	}
-	return s, nil
-}
-
-// AnalyzeHTTPResource analyze `https://` or `http://`
-func AnalyzeHTTPResource(dsn config.DSN) (*schema.Schema, error) {
-	s := &schema.Schema{}
-	req, err := http.NewRequest("GET", dsn.URL, nil)
-	if err != nil {
-		return s, errors.WithStack(err)
-	}
-	for k, v := range dsn.Headers {
-		req.Header.Add(k, v)
-	}
-	client := &http.Client{Timeout: time.Duration(10) * time.Second}
-	resp, err := client.Do(req)
-	if err != nil {
-		return s, errors.WithStack(err)
-	}
-	defer resp.Body.Close()
-	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(s); err != nil {
-		return s, errors.WithStack(err)
-	}
-	if err := s.Repair(); err != nil {
-		return s, errors.WithStack(err)
-	}
-	return s, nil
-}
-
-// AnalyzeJSON analyze `json://`
-func AnalyzeJSON(urlstr string) (*schema.Schema, error) {
-	s := &schema.Schema{}
-	splitted := strings.Split(urlstr, "json://")
-	file, err := os.Open(splitted[1])
-	if err != nil {
-		return s, errors.WithStack(err)
-	}
-	dec := json.NewDecoder(file)
-	if err := dec.Decode(s); err != nil {
-		return s, errors.WithStack(err)
-	}
-	if err := s.Repair(); err != nil {
-		return s, errors.WithStack(err)
-	}
-	return s, nil
-}
-
-// Deprecated
-func AnalyzeJSONString(str string) (*schema.Schema, error) {
-	return AnalyzeJSONStringOrFile(str)
-}
-
-// AnalyzeJSONStringOrFile analyze JSON string or JSON file
-func AnalyzeJSONStringOrFile(strOrPath string) (s *schema.Schema, err error) {
-	s = &schema.Schema{}
-	var buf io.Reader
-	if strings.HasPrefix(strOrPath, "{") {
-		buf = bytes.NewBufferString(strOrPath)
-	} else {
-		buf, err = os.Open(filepath.Clean(strOrPath))
-		if err != nil {
-			return s, errors.WithStack(err)
-		}
-	}
-	dec := json.NewDecoder(buf)
-	if err := dec.Decode(s); err != nil {
-		return s, errors.WithStack(err)
-	}
-	if err := s.Repair(); err != nil {
-		return s, errors.WithStack(err)
 	}
 	return s, nil
 }
--- datasource/datasource_test.go
+++ datasource/datasource_test.go
@@ -1,11 +1,8 @@
 package datasource
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
-	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/feitian124/goapi/config"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
@@ -19,29 +16,6 @@ var tests = []struct {
 }{
 	{config.DSN{URL: "my://root:mypass@localhost:33306/testdb"}, "testdb", 9, 6},
 	{config.DSN{URL: "pg://postgres:pgpass@localhost:55432/testdb?sslmode=disable"}, "testdb", 17, 12},
-	{config.DSN{URL: "json://../testdata/testdb.json"}, "testdb", 11, 12},
-	{config.DSN{URL: "https://raw.githubusercontent.com/k1LoW/tbls/main/testdata/testdb.json"}, "testdb", 11, 12},
-	{config.DSN{URL: "ms://SA:MSSQLServer-Passw0rd@localhost:11433/testdb"}, "testdb", 10, 7},
-}
-
-func TestMain(m *testing.M) {
-	cPath := credentialPath()
-	if _, err := os.Lstat(cPath); err == nil {
-		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", cPath)
-		bqTest := struct {
-			dsn           config.DSN
-			schemaName    string
-			tableCount    int
-			relationCount int
-		}{
-			config.DSN{URL: "bq://bigquery-public-data/bitcoin_blockchain"}, "bigquery-public-data:bitcoin_blockchain", 2, 0,
-		}
-		tests = append(tests, bqTest)
-	}
-	exit := m.Run()
-	if exit != 0 {
-		os.Exit(exit)
-	}
 }
 
 func TestAnalyzeSchema(t *testing.T) {
@@ -84,29 +58,4 @@ func TestAnalyzeRelations(t *testing.T) {
 			t.Errorf("got %v\nwant %v", got, want)
 		}
 	}
-}
-
-func TestAnalyzeJSONString(t *testing.T) {
-	b, err := os.ReadFile(filepath.Join(testdataDir(), "testdb.json"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	s, err := AnalyzeJSONString(string(b))
-	if err != nil {
-		t.Fatal(err)
-	}
-	if want := "testdb"; s.Name != want {
-		t.Errorf("got %v want %v", s.Name, want)
-	}
-}
-
-func credentialPath() string {
-	wd, _ := os.Getwd()
-	return filepath.Join(filepath.Dir(wd), "client_secrets.json")
-}
-
-func testdataDir() string {
-	wd, _ := os.Getwd()
-	dir, _ := filepath.Abs(filepath.Join(filepath.Dir(wd), "testdata"))
-	return dir
 }
